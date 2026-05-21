2fca5d6a255ba3f3064c457746920e01c8279854
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-05-20 20:15:03 -0400

    inicializando apicentral jeje

---
+++ src/test/java/com/sanosysalvos/apicentral/ApicentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.apicentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
