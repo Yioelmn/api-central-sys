d6e4651a2bcd7b4f2a996784cf843dd8c5b2061e
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-05-20 21:27:42 -0400

    ahí si, no habia llenado el repo jeje

--- src/main/java/com/sanosysalvos/apicentral/repository/ReporteRepository.java
+++ src/main/java/com/sanosysalvos/apicentral/repository/ReporteRepository.java
@@ -1,5 +1,18 @@
 package com.sanosysalvos.apicentral.repository;
 
-public interface ReporteRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.sanosysalvos.apicentral.model.ReporteModel;;
+
+@Repository
+public interface ReporteRepository extends JpaRepository<ReporteModel, Long>{
+
+    List<ReporteModel> findByTipoReporte(String tipoReporte);
+
+    List<ReporteModel> findByComuna(String comuna);
+
+    /*para cuando la api de mascotas se conecte a este
+    List<ReporteModel> findByMascotaId(Long mascotaId); */
 }
